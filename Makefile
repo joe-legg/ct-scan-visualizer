8e16afc0d094f250a817556e3b72cd73fadbab64
Joe Legg <joelegg3@gmail.com>
2024-03-14 20:05:19 +0000

    create array of vec3's representing a point cloud from image data

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC=clang
-CFLAGS=-o visualizer -g -Wall -lglfw -lGL -ltiff -Iinclude/
-SRC_FILES=visualizer.c glad.c
+CFLAGS=-o visualizer -g -Wall -lglfw -lGL -ltiff -I extern/include/
+SRC_FILES=visualizer.c extern/src/glad.c
 
 all:
 	$(CC) $(CFLAGS) $(SRC_FILES)
--- visualizer.c
+++ visualizer.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
-#include <stdlib.h>
 #include <stdbool.h>
 
 #include <tiffio.h>
 #include <glad/glad.h>
+#include <cglm/cglm.h>
 #include <GLFW/glfw3.h>
 
 #define LOG(...) fprintf(stderr, __VA_ARGS__)
@@ -16,10 +17,49 @@
 #define VERTEX_SHADER_PATH "./shaders/vertex.vert"
 #define FRAGMENT_SHADER_PATH "./shaders/fragment.frag"
 
-// Load images
+// types
 
-void load_tiff(const char *filename) {
+typedef struct {
+    GLFWwindow *window;
+    GLuint shader_program;
+} RenderContext;
+
+typedef struct {
+    size_t cap;
+    size_t length;
+    vec3 *data;
+} PointCloud;
+
+// load images
+
+PointCloud *point_cloud_init() {
+    PointCloud *point_cloud = malloc(sizeof(PointCloud));
+    point_cloud->length = 0;
+    point_cloud->cap = 4;
+    point_cloud->data = malloc(sizeof(vec3) * point_cloud->cap);
+    return point_cloud;
+}
+
+void point_cloud_free(PointCloud *point_cloud) {
+    free(point_cloud->data);
+    free(point_cloud);
+}
+
+void point_cloud_append(PointCloud *point_cloud, vec3 point) {
+    if (point_cloud->length == point_cloud->cap) {
+        point_cloud->cap *= 2;
+        point_cloud->data =
+            realloc(point_cloud->data, sizeof(vec3) * point_cloud->cap);
+    }
+
+    memcpy(point_cloud->data[point_cloud->length], point,
+           sizeof(vec3));
+    ++point_cloud->length;
+}
+
+void tiff_to_points(const char *filename, int z, PointCloud *point_cloud) {
     LOG("Reading TIFF file: %s\n", filename);
+
     TIFF *tif = TIFFOpen(filename, "r");
     if (!tif) {
         LOG("Failed to open TIFF file: %s\n", filename);
@@ -31,29 +71,34 @@ void load_tiff(const char *filename) {
     TIFFGetField(tif, TIFFTAG_IMAGELENGTH, &h);
 
     size_t pixels_count = w * h;
-    uint32_t *raster = malloc(pixels_count * sizeof(uint32_t));
+    uint32_t *raster = _TIFFmalloc(pixels_count * sizeof(uint32_t));
     if (!raster) {
         LOG("Failed to allocate memory to read TIFF file: %s\n", filename);
         exit(-1);
     }
 
+    // read the image
     if (!TIFFReadRGBAImage(tif, w, h, raster, 0)) {
         LOG("Failed to read TIFF RGBA data in file: %s\n", filename);
     }
+    TIFFClose(tif);
 
-    for (int i = 0; i < pixels_count; i++) {
-        int r = TIFFGetR(raster[i]);
-        int g = TIFFGetG(raster[i]);
-        int b = TIFFGetB(raster[i]);
-        int a = TIFFGetA(raster[i]);
+    // convert the raster to points
+    for (int y = 0; y < h; y++) {
+        for (int x = 0; x < w; x++) {
+            int raster_index = y * w + x;
+            int r = TIFFGetR(raster[raster_index]);
+            int g = TIFFGetG(raster[raster_index]);
+            int b = TIFFGetB(raster[raster_index]);
 
-        if (r || g || b) {
-            LOG("%d %d %d %d\n", r, g, b, a);
+            // only non-black pixels should be converted into points
+            if (r || g || b) {
+                vec3 point = {x, y, z};
+                point_cloud_append(point_cloud, point);
+            }
         }
     }
-
-    free(raster);
-    TIFFClose(tif);
+    _TIFFfree(raster);
 }
 
 void load_images(const char *path) {
@@ -64,6 +109,8 @@ void load_images(const char *path) {
         exit(-1);
     }
 
+    PointCloud *point_cloud = point_cloud_init();
+    float z = 0;
     for (int i = 0; i < file_count; i++) {
         struct dirent *file = file_list[i];
 
@@ -71,8 +118,11 @@ void load_images(const char *path) {
             char *dot = strrchr(file->d_name, '.');
             if (dot && strcmp(dot, ".tif") == 0) {
                 char file_path[strlen(path) + strlen(file->d_name) + 2];
-                snprintf(file_path, sizeof(file_path), "%s/%s", path, file->d_name);
-                load_tiff(file_path);
+                snprintf(file_path, sizeof(file_path), "%s/%s", path,
+                         file->d_name);
+
+                tiff_to_points(file_path, z, point_cloud);
+                ++z;
             }
         }
         free(file_list[i]);
@@ -81,12 +131,7 @@ void load_images(const char *path) {
     free(file_list);
 }
 
-// Rendering
-
-typedef struct {
-    GLFWwindow *window;
-    GLuint shader_program;
-} RenderContext;
+// rendering
 
 GLuint load_shader(const char *filename, GLenum shader_type) {
     // read file
@@ -141,11 +186,10 @@ RenderContext *init_renderer() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_SAMPLES, 4); // Anti-aliasing
+    glfwWindowHint(GLFW_SAMPLES, 4);  // Anti-aliasing
 
-    GLFWwindow *window = glfwCreateWindow(
-        WINDOW_WIDTH, WINDOW_HEIGHT, "CT Scan Visualizer", NULL, NULL
-    );
+    GLFWwindow *window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT,
+                                          "CT Scan Visualizer", NULL, NULL);
 
     glfwMakeContextCurrent(window);
     glfwSetWindowSizeCallback(window, window_size_callback);
@@ -161,7 +205,8 @@ RenderContext *init_renderer() {
 
     // load shaders
     GLuint vertex_shader = load_shader(VERTEX_SHADER_PATH, GL_VERTEX_SHADER);
-    GLuint fragment_shader = load_shader(FRAGMENT_SHADER_PATH, GL_FRAGMENT_SHADER);
+    GLuint fragment_shader =
+        load_shader(FRAGMENT_SHADER_PATH, GL_FRAGMENT_SHADER);
 
     // create the shader program
     GLuint shader_program = glCreateProgram();
@@ -178,7 +223,6 @@ RenderContext *init_renderer() {
         exit(-1);
     }
 
-    // TODO: delete shader program
     glDeleteShader(vertex_shader);
     glDeleteShader(fragment_shader);
 
@@ -190,6 +234,7 @@ RenderContext *init_renderer() {
 }
 
 void terminate_renderer(RenderContext *render_ctx) {
+    glDeleteProgram(render_ctx->shader_program);
     glfwDestroyWindow(render_ctx->window);
     glfwTerminate();
     free(render_ctx);
@@ -201,7 +246,7 @@ void render(RenderContext *render_ctx) {
     glfwSwapBuffers(render_ctx->window);
 }
 
-// Input
+// input
 
 void handle_input(GLFWwindow *window) {
     glfwPollEvents();
